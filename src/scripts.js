3f4c344e7cacb6404ce57a69dda1261784f45927
john <physitime@gmail.com>
2018-06-12 02:34:40 +0430

    add uikit js

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,6 @@
 /* eslint-disable import/no-webpack-loader-syntax */
+
 import "script-loader!jquery/dist/jquery";
 import 'script-loader!popper.js/dist/umd/popper.min.js';
 import "script-loader!bootstrap-material-design/dist/js/bootstrap-material-design";
+import "script-loader!uikit/dist/js/uikit.min" ;
