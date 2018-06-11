4cdef055467a485d187981a2d33faafd1fe50bf5
john <physitime@gmail.com>
2018-06-12 02:33:20 +0430

    add new method

--- src/components/common/loaders.js
+++ src/components/common/loaders.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as Spinners from "react-spinners";
+import TronIcon from "../../images/tron-icon-animated.svg";
 
 const DEFAULT_COLOR = "#343a40";
 
@@ -13,4 +14,19 @@ export function PropagateLoader(props = {}) {
   return (
     <Spinners.PropagateLoader color={DEFAULT_COLOR} size={20} {...props} />
   )
+}
+
+export function TronLoader({options = {}, children = null, height = 70, ...props}) {
+
+  return (
+    <div className="p-3 text-center">
+      <img src={TronIcon} style={{ height }} />
+      {
+        children && <div className="pt-3">
+          {children}
+        </div>
+      }
+
+    </div>
+  );
 }
